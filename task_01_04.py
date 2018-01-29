caf2f944e974df731c6a268a9afb674a763380e7
VVN888 <volkov_vladimir_13@mail.ru>
2018-01-29 16:16:20 +0300

    правка_03

--- task_01_04.py
+++ task_01_04.py
@@ -4,23 +4,23 @@ x2 = int(input()) # координата х точки В
 y2 = int(input()) # координата у точки В
 x3 = int(input()) # координата х точки С
 y3 = int(input()) # координата у точки С
-ab = (((x2-x1)**2)+((y2-y1)**2))**(1/2) # сторона AB
-ac = (((x3-x1)**2)+((y3-y1)**2))**(1/2) # сторона AC
-bc = (((x3-x2)**2)+((y3-y2)**2))**(1/2) # сторона AC
-# print(ab)
-# print(ac)
-# print(bc)
-if ab >ac and ab> bc:
-    n1 = ab**2
-    n2 = ac**2 + bc**2
+ab2 = ((x2-x1)**2)+((y2-y1)**2) # сторона AB в квадрате
+ac2 = ((x3-x1)**2)+((y3-y1)**2) # сторона AC в квадрате
+bc2 = ((x3-x2)**2)+((y3-y2)**2) # сторона AC в квадрате
+# print(ab2)
+# print(ac2)
+# print(bc2)
+if ab2 > ac2 and ab2 > bc2:
+    n1 = ab2
+    n2 = ac2 + bc2
 #    print('АВ - гипотенуза')
-if ac >ab and ac> bc:
-    n1 = ac**2
-    n2 = ab**2 + bc**2
+if ac2 > ab2 and ac2 > bc2:
+    n1 = ac2
+    n2 = ab2 + bc2
 #    print('АС - гипотенуза')
-if bc >ab and bc> ac:
-    n1 = bc**2
-    n2 = ab**2 + ac**2
+if bc2 > ab2 and bc2 > ac2:
+    n1 = bc2
+    n2 = ab2 + ac2
 #    print('ВС - гипотенуза')
 if n1 == n2:
     print('yes')

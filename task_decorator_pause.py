3da91d8182b84c99935357a7696cc45ba8658e74
VVN888 <volkov_vladimir_13@mail.ru>
2018-02-26 17:11:29 +0300

    правки

--- task_decorator_pause.py
+++ task_decorator_pause.py
@@ -3,11 +3,11 @@ import time
 
 def pause(f):
     time.sleep(2)
-    return f   #(*args, **kwargs)
+    return str(f)   #(*args, **kwargs), добавил тип str
 
 @pause   # 1-й вар-т реализации декоратора
-def func():
+def func(*args, **kwargs):
     pass
 print('Фунция выполняется с задержкой 2 секунды!')
 # func = pause(func)   # 2-й вар-т реализации декоратора
-# print('Фунция выполняется с задержкой 2 секунды!')
+#print('Фунция выполняется с задержкой 2 секунды!')
